2ed0ceff4a6e70ea18eb178654e38e40f4b11005
DotnetSahooKashi <sahookahsinath92@gmail.com>
2018-03-20 13:33:53 +0530

    Added the Changes to add the security on the internal Services of RTLS with the A8Platform and External Services

---
+++ RTLS.Domins/Identity/ApplicationUser.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace RTLS.Domins.Identity
+{
+    //[Validator(typeof(UserValidator))]
+    public class ApplicationUser : IdentityUser<int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUser<int>
+    {
+
+        public ApplicationUser()
+        {
+            CreationDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser,int> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public async Task<ClaimsIdentity>
+            GenerateUserIdentityAsync(UserManager<ApplicationUser, int> manager)
+        {
+            var userIdentity = await manager
+                .CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+        [Required()]
+        public override string UserName { get; set; }
+
+        public string ForeName { get; set; }
+
+        public string LastName { get; set; }
+        public override string Email { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime  UpdateDate { get; set; }
+
+        public int? SiteId { get; set; }
+      
+        [MaxLength(50)]
+        public string Status { get; set; }
+
+    }
+}
---
+++ RTLS.Domins/Identity/ApplicationUserStore.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace RTLS.Domins.Identity
+{
+    public class ApplicationUserStore :
+     UserStore<ApplicationUser, ApplicationRole, int,
+     ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUserStore<ApplicationUser, int>, IDisposable
+    {
+        public ApplicationUserStore()
+            : base(new A8AdminDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationUserStore(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override Task CreateAsync(ApplicationUser user)
+        {
+            return base.CreateAsync(user);
+        }
+    }
+}
