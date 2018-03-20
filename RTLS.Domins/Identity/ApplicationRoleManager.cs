7980b0d572594dbe39c0758b33f9ad593f13f31a
DotnetSahooKashi <sahookahsinath92@gmail.com>
2018-03-20 13:33:53 +0530

    Added the Changes to add the security on the internal Services of RTLS with the A8Platform and External Services

---
+++ RTLS.Domins/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace RTLS.Domins.Identity
+{
+
+    // PASS CUSTOM APPLICATION ROLE AND INT AS TYPE ARGUMENTS TO BASE:
+    public class ApplicationRoleManager : RoleManager<ApplicationRole, int>
+    {
+        // PASS CUSTOM APPLICATION ROLE AND INT AS TYPE ARGUMENTS TO CONSTRUCTOR:
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, int> roleStore)
+            : base(roleStore)
+        {
+        }
+
+        // PASS CUSTOM APPLICATION ROLE AS TYPE ARGUMENT:
+        public static ApplicationRoleManager Create(
+            IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            return new ApplicationRoleManager(
+                new ApplicationRoleStore(context.Get<A8AdminDbContext>()));
+        }
+    }
+
+}
