55445a7c01f7f5f65a70e8c83fd0ce1c8f1974fb
somanath mishra <somanath.mishra@planetsbrain.com>
2018-06-18 13:12:14 +0530

    modified to take backup and clear RTLS data...........

---
+++ RTLS.Domins/FilterLocationData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTLS.Domins
+{
+    public class FilterLocationData
+    {
+        public string MacAddress { get; set; }
+
+        public string AreaName { get; set; }
+        public int RecordToDisply { get; set; }
+        /// <summary>
+        /// First record that should be shown(used for paging)
+        /// </summary>
+        public int TotalPage { get; set; }
+        /// <summary>
+        /// Number of columns in table
+        /// </summary>
+        public int CurrentPage { get; set; }
+        public int TotalRecords { get; set; }
+        public string SiteName { get; set; }
+        public int SiteId { get; set; }
+        public int TimeFrame { get; set; }
+    }
+}
