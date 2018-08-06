9f23a5b4dc441aa672f30f4dff369040fe222a3a
somanath mishra <somanath.mishra@planetsbrain.com>
2018-08-06 21:53:30 +0530

    Added RtlS.Common project with Secom Login and register api changes..

---
+++ RTLS.Domins/ViewModels/ReturnData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTLS.Domins.ViewModels
+{
+   public class ReturnData
+   {
+
+        public string Token { get; set; }
+
+   }
+}
