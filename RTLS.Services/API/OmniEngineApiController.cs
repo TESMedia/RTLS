cec96fef146652b0b206af5831cc480109625779
DotnetSahooKashi <sahookahsinath92@gmail.com>
2018-08-06 21:44:41 +0530

    Added the changes to create the post API to get the Macaddress,SiteId and NotificationType in the request body from captive portal

---
+++ RTLS.Services/API/OmniEngineApiController.cs
@@ -0,0 +1,25 @@
+﻿using RTLS.Domins.ViewModels.OmniRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RTLS.API
+{
+    [RoutePrefix("api/OmniEngine")]
+    public class OmniEngineApiController : ApiController
+    {
+        public OmniEngineApiController()
+        {
+
+        }
+        
+        [Route("RegisterDevice")]
+        public HttpResponseMessage AddDevice(RequestOmniModel objRequestOmniModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
